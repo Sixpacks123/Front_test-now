6a2bda9638f5cad087d48bf554068514d76ce2a6
Sixpacks123 <aub.heurtault@gmail.com>
2021-10-29 19:22:56 +0200

    ajout du template pour les reviews

--- components/Reviews/ReviewsContent.vue
+++ components/Reviews/ReviewsContent.vue
@@ -1,5 +1,12 @@
 <template>
-    <section>
+    <section class="section-color">
+        <div class="page-container">
+            <h2 class="font-police">Retours de nos clients</h2>
+            <div class="content-reviews"> <!-- bloc flex-box pour les commentaire -->
+                <ReviewsCard/>
+            </div>
+        </div>
+        
     </section>
 </template>
 <style scoped>
