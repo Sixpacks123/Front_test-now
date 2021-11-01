9752dc2a09ff7de17401eafabfa03a759c9ccf4d
Sixpacks123 <aub.heurtault@gmail.com>
2021-11-01 15:25:41 +0100

    ajout structure de FavContent

--- components/Fav/FavContent.vue
+++ components/Fav/FavContent.vue
@@ -2,9 +2,31 @@
     <section class="section-colors">
         <div class="page-container">
         <h2 class="font-police">Nos coups de coeur</h2>
+            <div>
+                <FavHeartCard/>
+            </div>
         </div>            
     </section>    
 </template>
+<script>
+export default {
+    data () {
+        return {
+            items: [
+                {
+                    title: '',
+                    country: '',
+                    town: '',
+                    price: '',
+                    popular: '',
+                    time:'',
+                    rating:''
+                }
+            ]
+        }
+    }
+}
+</script>
 <style scoped>
 
 </style>
